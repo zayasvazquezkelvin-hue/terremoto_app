9f22e3f68ce0063025d92f8493b70bbe003d4719
zayasvazquezkelvin-hue <zayasvazquezkelvin@gmail.com>
2025-11-26 19:27:55 +0000

    mapa y tabla

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,35 @@
 import streamlit as st
+import pandas as pd
+from quakefeeds import QuakeFeed
+import plotly.express as px
+st.title("Terremoto App")
 
-st.title("Mi primer app")
+feed = QuakeFeed("2.5", "month")
 
+token_id = "pk.eyJ1IjoibWVjb2JpIiwiYSI6IjU4YzVlOGQ2YjEzYjE3NTcxOTExZTI2OWY3Y2Y1ZGYxIn0.LUg7xQhGH2uf3zA57szCyw"
+
+px.set_mapbox_access_token(token_id)
+
+longitudes = [feed.location (i) [0] for i in range(len(feed))]
+
+latitudes = [feed.location (i) [1] for i in range(len(feed))]
+
+date = list(feed.event_times)
+
+depths = list(feed.depths)
+
+places = list(feed.places)
+
+magnitudes = list(feed.magnitudes)
+
+df = pd.DataFrame([date, longitudes, latitudes, places, magnitudes, depths]).transpose()
+df.columns = ["Fecha", "lon", "lat", "loc", "mag", "prof"]
+
+df["lat"] = pd.to_numeric(df["lat"])
+df["lon"] = pd.to_numeric(df["lon"])
+df["mag"] = pd.to_numeric(df["mag"])
+df["prof"] = pd.to_numeric(df["prof"])
+
+fig = px.scatter_mapbox(df, lat = "lat", lon = "lon", color = "mag", size = "mag", mapbox_style= "dark", center= dict(lat=18.25178, lon=-66.254512))
+
+st.write(fig)
