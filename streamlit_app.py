41b7dc0baa3f37bddce5ebb614fb6f1935374264
zayasvazquezkelvin-hue <zayasvazquezkelvin@gmail.com>
2025-12-13 22:09:41 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ def generaTabla():
 # Formatear fecha
 # ---------------
 
-    df["Fecha "] = df["Fecha"].dt.strftime("%d de %B de %Y", locale='Spanish_Spain').str.capitalize()
+    df["Fecha "] = df["Fecha"].dt.strftime("%d de %B de %Y").str.capitalize()
     return df
 
 # ----------------
